10ca0464dfc0e27677300176f96893720cc90278
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-30 15:01:35 +0800

    Implement TextGeneratedEffect from Aceternity UI

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotless'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 function Hero() {
   return (
@@ -17,6 +18,29 @@ function Hero() {
           className='top-28 left-80 h-[80vh] w-[50vw]'
           fill='white'
         />
+      </div>
+      <div className='h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.09] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0'>
+        {/* Radial gradient for the container to give a faded look */}
+        <div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]' />
+      </div>
+
+      <div className='flex justify-center relative my-20 z-10'>
+        <div
+          className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-ce
+         justify-center'
+        >
+          <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+            Dynamic Web Magic With Next.Js
+          </h2>
+          <TextGenerateEffect
+            words='Transforming Concepts Into Seamless Experiences'
+            className='text-center text-[40px] md:text:5xl lg:text-6xl'
+            duration={0.3}
+          />
+          <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+            Hi I&apos;m Ismail, a NextJs developer based in Singapore
+          </p>
+        </div>
       </div>
     </div>
   )
